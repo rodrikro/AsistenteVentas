759200433b6a67cf1963e0c668ed8295cb34838b
Rodrigo Monarrez <rodrigokrotzsch@hotmail.com>
2022-05-30 00:01:48 -0500

    Se agrego la entidad de producto con información de inventario

---
+++ AccesoDatos/AccesoDatos/Modelos/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Modelos
+{
+    public class Producto
+    {
+
+        public int IdProducto { get; set; }
+        public int IdDepartamento { get; set; }
+        public int IdCategoria { get; set; }
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Presentacion { get; set; }
+        public int CantidadMinima { get; set; }
+        public int CantidadActual { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion{ get; set; }
+    }
+}
